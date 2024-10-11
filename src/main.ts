0726762cffb731c714dce21816cecd7c5dcfc61e
xanedesu <xanedesu@gmail.com>
2024-10-11 14:39:38 +0300

    fastify and deps

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
+import helmet from '@fastify/helmet';
 import { ValidationPipe, VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { FastifyAdapter } from '@nestjs/platform-fastify';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 import { AppConfigService } from './configs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, new FastifyAdapter());
+  const adapter = new FastifyAdapter();
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    adapter,
+  );
+
+  await app.register(helmet, {
+    contentSecurityPolicy: false,
+  });
+
+  app.enableCors();
 
   app.setGlobalPrefix('api');
   app.enableVersioning({
